997c83d6276b6981b9c241b3735ee390c04793fc
LJoki06 <leo.jokijarvi@elev.ga.ntig.se>
2025-08-22 10:44:02 +0200

    gjorde programmet

---
+++ prog.py
@@ -0,0 +1,21 @@
+import time
+
+name = input("Hej! Vad heter du?: ")
+matval = input("""Vad vill du ha för mat?:
+1. Pizza
+2. Hamburgare
+3. Korv
+""")
+if matval == "1":
+    print("Mmmm... Pizza... Yummy!!")
+if matval == "2":
+    print(f"Bra val {name}! Burgare är så gott!")
+if matval == "3":
+    time.sleep(0.5)
+    print("hehe...")
+    time.sleep(1)
+    print("DET ÄR VAD JAG GAV DIN MAMMA IGÅR NATT!!!!!!!!")
+    time.sleep(2)
+    print("...")
+    time.sleep(1)
+    print("Jag har en otroligt sofistikerad humor")
